7a36ab955d7917fb29fa416ed28f03c8da974263
Aadharsh Nuckhir <aadharshnuckhir@gmail.com>
2023-11-22 23:15:15 +0400

    Project creation.

---
+++ Customer.Microservice/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customer.Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly List<Models.Customer> _customers = new List<Models.Customer>
+    {
+        new Models.Customer { Id = 1, Name = "John Doe", Email = "john@example.com" },
+        new Models.Customer { Id = 2, Name = "Jane Smith", Email = "jane@example.com" }
+        // Add more dummy data as needed
+    };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Models.Customer>> GetCustomers()
+        {
+            return Ok(_customers);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Models.Customer> GetCustomerById(int id)
+        {
+            var customer = _customers.Find(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound(); // Return 404 if customer with given id is not found
+            }
+            return Ok(customer);
+        }
+    }
+}
---
+++ Product.Microservice/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Product.Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly List<Models.Product> _products = new List<Models.Product>
+    {
+        new Models.Product { Id = 1, Name = "Product A", Price = 19.99M },
+        new Models.Product { Id = 2, Name = "Product B", Price = 29.99M }
+        // Add more dummy data as needed
+    };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Models.Product>> GetProducts()
+        {
+            return Ok(_products);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Models.Product> GetProductById(int id)
+        {
+            var product = _products.Find(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound(); // Return 404 if product with given id is not found
+            }
+            return Ok(product);
+        }
+    }
+}
